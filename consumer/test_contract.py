abd2ea8e2945428d449f23ec3e7538397cb793a9
KirillKornakov <kirill.kornakov@atom.team>
2025-06-20 12:29:07 +0300

    update_contract_v2

--- consumer/test_contract.py
+++ consumer/test_contract.py
@@ -10,7 +10,7 @@ atexit.register(pact.stop_service)
 
 
 def test_get_user():
-    expected = {"id": 1, "name": "John Doe"}
+    expected = {"id": 1, "name": "John Doe", "title": "qa"}
 
     # Define the interaction
     (
